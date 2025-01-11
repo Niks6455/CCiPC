538cadb9c201b3184d72bc4a3429ad65a62e7b50
Никита <100412266+Niks6455@users.noreply.github.com>
2025-01-11 03:34:31 +0300

    fix

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,33 +1,34 @@
 import styles from "./Header.module.scss";
+
 function Header() {
     return ( 
         <header>
-        <div className={styles.HeaderContainer}>
-            <div className={styles.logo}>
-                <img src="/img/logo.svg" alt="logo"/>
+            <div className={styles.HeaderContainer}>
+                <div className={styles.logo}>
+                    <img src="/img/logo.svg" alt="logo" />
+                </div>
+                <div className={styles.TitleText}>
+                    <img src="/img/text.svg" alt="Conference Title" />
+                </div>
             </div>
-            <div className={styles.TitleText}>
-                {/* <img src="/img/numb.svg" className={styles.number}/> */}
-                {/* <p>
-                    ВСЕРОССИЙСКАЯ НАУЧНАЯ КОНФЕРЕНЦИЯ "СИСТЕМНЫЙ СИНТЕЗ И ПРИКЛАДНАЯ СИНЕРГЕТИКА"
-                </p> */}
-                <img src="/img/text.svg"/>
+            <div className={styles.HeaderMenuContainer}>
+                <div className={styles.HeaderMenu}>
+                    <ul>
+                        <li>Автору <span className={styles.arowLi}><img src="/img/ArrowMenu.png" alt="Arrow" /></span></li>
+                        <li>Участнику <span className={styles.arowLi}><img src="/img/ArrowMenu.png" alt="Arrow" /></span></li>
+                        <li>Оргкомитет <span className={styles.arowLi}><img src="/img/ArrowMenu.png" alt="Arrow" /></span></li>
+                        <li>Вход/Регистрация <span className={styles.arowLi}><img src="/img/ArrowMenu.png" alt="Arrow" /></span></li>
+                    </ul>
+                </div>
+                <div className={styles.HeaderLocarion}>
+                        <p>Ru</p>
+                        <img src="/img/arrow.svg" alt="Language Selector" />
+                    </div>
+             
             </div>
-        </div>
-        <div className={styles.HeaderMenu}>
-            <ul>
-                <li>Автору <span className={styles.arowLi}><img src="/img/ArrowMenu.png"/></span></li>
-                <li>Участнику <span className={styles.arowLi}><img src="/img/ArrowMenu.png"/></span></li>
-                <li>Оргкомитет <span className={styles.arowLi}><img src="/img/ArrowMenu.png"/></span></li>
-                <li>Вход/Регистрация <span className={styles.arowLi}><img src="/img/ArrowMenu.png"/></span></li>
-            </ul>
-        </div>
-        <div className={styles.HeaderLocarion}>
-            <p>Ru</p>
-            <img src="/img/arrow.svg"/>
-        </div>
+            
         </header>
-     );
+    );
 }
 
 export default Header;
--- front/src/modules/TopMainInfo/TopMainInfo.jsx
+++ front/src/modules/TopMainInfo/TopMainInfo.jsx
@@ -5,37 +5,41 @@ function TopMainInfo() {
     return ( 
         <div className={styles.topContainer}>
             <div className={styles.TopHeader}>
-                <img src="/img/BgHomePageTop.png"/>
+                <img src="/img/Bg.jpeg" alt="Background" />
             </div>
-            <main >
-                <div  className={styles.mainTopTextContainer}>
+            <main>
+                <div className={styles.mainTopTextContainer}>
                     <div className={styles.mainTopText}>
                         <div className={styles.mainTopDate}>
-                                <p>23 - 29 сентября 2025 года <br/>пос. Нижний Архыз</p>
-                            </div>
-                            <div className={styles.mainTopList}>
-                                <ul>
-                                    <li>
-                                        Программа <br/>конференции <span className={styles.clicker}></span>
-                                    </li>
-                                    <li>
-                                        Информационное <br/>письмо <span className={styles.clicker}></span>
-                                    </li>
-                                    <li>
-                                        Последний сборник <br/>научных трудов <span className={styles.clicker}></span>
-                                    </li>
-                                </ul>
-                            </div>
-                    </div>
-                 
-                    <div className={styles.buttonClicker}>
-                        <Button text={"Подать заявку"}/>
+                            <p>23 - 29 сентября 2025 года <br /> пос. Нижний Архыз</p>
+                        </div>
+                        <div className={styles.mainTopList}>
+                            <ul>
+                                <li>
+                                    Программа <br /> конференции <span className={styles.clicker}></span>
+                                </li>
+                                <li>
+                                    Информационное <br /> письмо <span className={styles.clicker}></span>
+                                </li>
+                                <li>
+                                    Последний сборник <br /> научных трудов <span className={styles.clicker}></span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-              
+                <div className={styles.buttonClicker}>
+                    <div className={styles.buttonClickerInner}>
+                        <img className={styles.leftBot} src="/img/rect.png" alt="Background"/>
+                        <Button text={"Подать заявку"} />
+                        {/* <img className={styles.leftTop} src="/img/rect.png" alt="Background"/> */}
+
+                    </div>
+                  
+                    </div>
             </main>
         </div>
-     );
+    );
 }
 
 export default TopMainInfo;
