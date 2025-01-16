643d4e9152958d8d222543d950006cb80307f6d8
forellko <45753449+Forellko@users.noreply.github.com>
2025-01-16 17:45:08 +0300

    Create buttons

--- front/src/pages/Author/Author.jsx
+++ front/src/pages/Author/Author.jsx
@@ -1,29 +1,67 @@
-import React, { useContext, useState } from "react";
-import Layout from "../../ui/Layout/Layout";
-import Footer from "../../components/Footer/Footer";
-import styles from "./Author.module.scss";
-import { use } from "react";
-import DataContext from "../../context";
-import ParticipationConference from "../../modules/ParticipationConference/ParticipationConference";
-import Collections from "../../modules/Collections/Collections";
-import HeaderSecond from "../../components/HeaderSecond/HeaderSecond";
+import React, { useState } from 'react';
+import Layout from '../../ui/Layout/Layout';
+import Footer from '../../components/Footer/Footer';
+import styles from './Author.module.scss';
 function Author() {
-    const context = useContext(DataContext);
-    return ( 
-        <main className={styles.Author}>
-            {/* <HeaderSecond/> */}
-            <Layout>
-                    <div className={styles.AuthorContainer}>
-                        <div>
-                            <button onClick={() => context.setActiveModule(false)}>1 модуль</button>
-                            <button onClick={() => context.setActiveModule(true)}>2 модуль</button>
-                        </div>
-                        {!context.activeModule ? <ParticipationConference/> : <Collections/>}
-                    </div>
-            </Layout>
-            <Footer/>
-        </main>
-     );
+  const [selectedButton, setSelectedButton] = useState('Registration');
+
+  return (
+    <main>
+      <Layout>
+        <div className={styles.author}>
+          <h1 className={styles.h1}>заявка на участие</h1>
+          <div className={styles.buttons}>
+            <div
+              className={`${styles.button} ${
+                selectedButton === 'Registration'
+                  ? styles.button_active
+                  : styles.button_passive
+              }`}
+              onClick={() => setSelectedButton('Registration')}
+            >
+              <span className={`${styles.button_text}`}>
+                Оформление участия в конференции
+              </span>
+              <span className={styles.cap_active}></span>
+            </div>
+            <div
+              className={`${styles.button} ${
+                selectedButton === 'Collection'
+                  ? styles.button_active
+                  : styles.button_passive
+              }`}
+              onClick={() => setSelectedButton('Collection')}
+            >
+              <span>Сборники прошлых лет</span>
+              <span className={styles.book_active}></span>
+            </div>
+          </div>
+          {/* first */}
+          {selectedButton === 'Registration' && (
+            <div>
+              <p>
+                Для участия в конференции необходимо зарегистрироваться на
+                платформе и подать заявку, заполнив регистрационную форму в
+                личном кабинете. В срок до XX.XX.XX необходимо прислать заявку
+                на доклад, заполнив обязательные поля, а в срок до ХХ.ХХ.ХХХХ
+                загрузить статью и экспертное заключение.
+              </p>
+              <p>При подаче заявки прикрепляются:</p>
+              <p>1) доклад, оформленный по шаблону в Word;</p>
+              <p>
+                2) файл с отсканированным изображением экспертного заключения о
+                возможности публикации с подписью председателя экспертной
+                комиссии и печатью организации.
+              </p>
+            </div>
+          )}
+          {/* second */}
+          <div>{selectedButton === 'Collection' && <div>lol</div>}</div>
+        </div>
+      </Layout>
+      <Footer />
+    </main>
+  );
 }
 
 export default Author;
