6c0779efffe294c9136211983effe78d2820be96
forellko <45753449+Forellko@users.noreply.github.com>
2025-01-17 18:07:29 +0300

    Connect CommitteesPage

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState } from "react";
-import HomePage from "./pages/HomePage/HomePage";
-import DataContext from "./context";
-import "./styles/app.css";
-import AuthPage from "./pages/AuthPage/AuthPage";
-import NoteFoundPage from "./pages/NoteFoundPage/NoteFoundPage";
-import NewsPage from "./pages/NewsPage/NewsPage";
-import Author from "./pages/Author/Author";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import HomePage from './pages/HomePage/HomePage';
+import DataContext from './context';
+import './styles/app.css';
+import AuthPage from './pages/AuthPage/AuthPage';
+import NoteFoundPage from './pages/NoteFoundPage/NoteFoundPage';
+import NewsPage from './pages/NewsPage/NewsPage';
+import Author from './pages/Author/Author';
+import CommitteesPage from './pages/CommitteesPage/CommitteesPage';
 function App() {
-
-  const [authPage, setAuthPage] = useState("Auth");
-  const [mailValue, setMailValue] = useState("test@sfedu.ru");
+  const [authPage, setAuthPage] = useState('Auth');
+  const [mailValue, setMailValue] = useState('test@sfedu.ru');
   const [activeModule, setActiveModule] = useState(false);
 
   const context = {
@@ -19,27 +19,25 @@ function App() {
     setMailValue,
     mailValue,
     activeModule,
-    setActiveModule
+    setActiveModule,
   };
 
-
   return (
-    <DataContext.Provider
-  value={context}
-  >
-    <BrowserRouter>
-      <main style={{ fontFamily: "REX" }}>
-        <Routes>
-          <Route path="/" element={<AuthPage />}></Route>
-          <Route path="/HomePage" element={<HomePage />}></Route>
-          <Route path="/NewsPage" element={<NewsPage />}></Route>
-          <Route path="/Author" element={<Author />}></Route>
-          <Route path="*" element={<NoteFoundPage />} /> {/* Добавление 404 страницы */}
-          
-        </Routes>
-      </main>
-    </BrowserRouter>
-  </DataContext.Provider>
+    <DataContext.Provider value={context}>
+      <BrowserRouter>
+        <main style={{ fontFamily: 'REX' }}>
+          <Routes>
+            <Route path="/" element={<AuthPage />}></Route>
+            <Route path="/HomePage" element={<HomePage />}></Route>
+            <Route path="/NewsPage" element={<NewsPage />}></Route>
+            <Route path="/Author" element={<Author />}></Route>
+            <Route path="/Committees" element={<CommitteesPage />} />
+            <Route path="*" element={<NoteFoundPage />} />{' '}
+            {/* Добавление 404 страницы */}
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </DataContext.Provider>
   );
 }
 
