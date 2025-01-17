fee71386ab03d3cd5779069d42cebbc1e81dbcb2
forellko <45753449+Forellko@users.noreply.github.com>
2025-01-17 19:05:42 +0300

    Add buttons to page

--- front/src/pages/CommitteesPage/CommitteesPage.jsx
+++ front/src/pages/CommitteesPage/CommitteesPage.jsx
@@ -1,30 +1,75 @@
+import Layout from '../../ui/Layout/Layout';
+import Footer from '../../components/Footer/Footer';
+import styles from './CommitteesPage.module.scss';
+import ChangeButtons from '../../ui/ChangeButtons/ChangeButtons';
+import { useEffect, useState } from 'react';
 export default function CommitteesPage() {
+  const [setOrganizationComite, setProgrammingComite] = useState(0);
+
   return (
     <div>
-      {/* organization commite */}
-      <div>
-        <h2></h2>
-        {/* buttons */}
-        <div>
-          <div></div>
-          <div></div>
-        </div>
-        {/* images */}
-        <div>[].map()</div>
-      </div>
-      {/* programming comite */}
-      <div>
-        <h2></h2>
-        {/* buttons */}
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        {/* images */}
-        <div>[].map()</div>
-      </div>
+      <Layout>
+        <main>
+          {/* organization commite */}
+          <div className={styles.organizationComite}>
+            <h2>ОРГАНИЗАЦИОННЫЙ КОМИТЕТ</h2>
+            {/* buttons */}
+            <div className={styles.organizationComiteButtons}>
+              <div></div>
+              <ChangeButtons
+                buttonArray={[
+                  {
+                    text: 'Сопредседатели',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                  {
+                    text: 'Сопредседатели',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                ]}
+                setIndex={setProgrammingComite}
+              />
+            </div>
+            {/* images */}
+            <div className={styles.organizationComiteImages}>
+              {[].map(() => 0)}
+            </div>
+          </div>
+          {/* programming comite */}
+          <div className={styles.programmingComite}>
+            <h2>ПРОГРАММНЫЙ КОМИТЕТ</h2>
+            {/* buttons */}
+            <div className={styles.programmingComiteButtons}>
+              <ChangeButtons
+                buttonArray={[
+                  {
+                    text: 'Почетный председатель',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                  {
+                    text: 'Cопредседатели',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                  {
+                    text: 'Заместитель председателя',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                  {
+                    text: 'Члены комитета',
+                    Icon: () => <img src="/img/Cap.svg" alt="Cap" />,
+                  },
+                ]}
+                setIndex={setProgrammingComite}
+              />
+            </div>
+            {/* images */}
+            <div className={styles.programmingComiteImages}>
+              {[].map(() => 0)}
+            </div>
+          </div>
+        </main>
+      </Layout>
+      <Footer />
     </div>
   );
 }
