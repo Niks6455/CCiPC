8d4079e2c36e1b9d498fb11161d4f211eb4182d8
forellko <45753449+Forellko@users.noreply.github.com>
2025-01-17 18:02:34 +0300

    Start CommitteesPage

---
+++ front/src/pages/CommitteesPage/CommitteesPage.jsx
@@ -0,0 +1,30 @@
+export default function CommitteesPage() {
+  return (
+    <div>
+      {/* organization commite */}
+      <div>
+        <h2></h2>
+        {/* buttons */}
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        {/* images */}
+        <div>[].map()</div>
+      </div>
+      {/* programming comite */}
+      <div>
+        <h2></h2>
+        {/* buttons */}
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        {/* images */}
+        <div>[].map()</div>
+      </div>
+    </div>
+  );
+}
