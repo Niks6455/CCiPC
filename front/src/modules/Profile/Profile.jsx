c83685cc13e497c7fb577aad751a77811b2fec5b
Никита <100412266+Niks6455@users.noreply.github.com>
2025-01-20 13:00:18 +0300

    sd

--- front/src/modules/Profile/Profile.jsx
+++ front/src/modules/Profile/Profile.jsx
@@ -5,8 +5,15 @@ function Profile() {
         <section className={styles.Profile}>
             <div>
                 <img src="/img/ProfilePictureBackground.svg" className={styles.ProfileImg}/>
+                <img src="/img/noPhotoLk.svg" className={styles.noPhotoLk}/>
             </div>
-            <p>Profile</p>
+            <div className={styles.mainSection}>
+                 <div className={styles.mainSectionInfoPeople}>
+                    <p>Веселов Геннадий Евгеньевич</p>
+                    <p><span>Ученое звание:</span> Доцент</p>
+                    <p><span>Степень:</span> Доктор технических наук</p>
+                 </div>
+            </div>
         </section>
      );
 }
