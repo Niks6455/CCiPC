6ac28b540d8a04120caa7d326cf8865117983dd9
Artur <artur.tonoyan.04@list.ru>
2025-02-11 19:24:18 +0300

    api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,77 +1,102 @@
-import {AppErrorInvalid, AppErrorMissing} from "../utils/errors.js";
+import { AppErrorInvalid, AppErrorMissing } from "../utils/errors.js";
 import authService from "../services/auth.js";
 import bcrypt from "bcrypt";
 import randomCode from "../utils/random-code.js";
 const atLeastOneDigit = /\d/,
-    atLeastOneLowerLetter = /[a-z]/,
-    atLeastOneUpperLetter = /[A-Z]/,
-    atLeastOneSpecial = /[!@_#$%^&*]/,
-    otherChars = /[^0-9a-zA-Z!@_#$%^&*]/g;
+  atLeastOneLowerLetter = /[a-z]/,
+  atLeastOneUpperLetter = /[A-Z]/,
+  atLeastOneSpecial = /[!@_#$%^&*]/,
+  otherChars = /[^0-9a-zA-Z!@_#$%^&*]/g;
 
 export default {
+  async login({ body: { email, password } }, res) {
+    if (!email) throw new AppErrorMissing("email");
+    if (!password) throw new AppErrorMissing("password");
 
-    async login({body: {email, password}}, res) {
+    const { participant, token } = await authService.login(email, password);
+    res.json({ participant, token: token });
+  },
 
-        if(!email) throw new AppErrorMissing('email')
-        if(!password) throw new AppErrorMissing('password')
-
-        const {participant, token} = await authService.login(email, password);
-        res.json({ participant, token: token });
+  async register(
+    {
+      body: {
+        email,
+        password,
+        name,
+        surname,
+        patronymic,
+        academicTitle,
+        degree,
+        position,
+        organization,
+        phone,
+      },
     },
+    res
+  ) {
+    if (!email) throw new AppErrorMissing("email");
+    if (!password) throw new AppErrorMissing("password");
 
+    const isValid =
+      atLeastOneDigit.test(password) &&
+      atLeastOneLowerLetter.test(password) &&
+      atLeastOneUpperLetter.test(password) &&
+      atLeastOneSpecial.test(password) &&
+      !otherChars.test(password) &&
+      password.length >= 8 &&
+      password.length <= 20;
 
-    async register({ body: {email, password, name, surname, patronymic, academicTitle, degree, position, organization, phone} }, res) {
-        if(!email) throw new AppErrorMissing('email')
-        if(!password) throw new AppErrorMissing('password')
+    if (!isValid) throw new AppErrorInvalid("password");
 
-        const isValid =
-            atLeastOneDigit.test(password) &&
-            atLeastOneLowerLetter.test(password) &&
-            atLeastOneUpperLetter.test(password) &&
-            atLeastOneSpecial.test(password) &&
-            !otherChars.test(password) &&
-            password.length >= 8 &&
-            password.length <= 20;
+    if (!name) throw new AppErrorMissing("name");
+    if (!surname) throw new AppErrorMissing("surname");
+    if (!academicTitle) throw new AppErrorMissing("academicTitle");
+    if (!degree) throw new AppErrorMissing("degree");
+    if (!position) throw new AppErrorMissing("position");
+    if (!organization) throw new AppErrorMissing("organization");
+    if (!phone) throw new AppErrorMissing("phone");
 
-        if (!isValid) throw new AppErrorInvalid('password');
+    const code = randomCode(6, "0123456789");
+    console.log("code", code);
+    const hashPassword = bcrypt.hashSync(password, 10);
+    const participant = await authService.register(
+      {
+        email,
+        hashPassword,
+        name,
+        surname,
+        patronymic,
+        academicTitle,
+        degree,
+        position,
+        organization,
+        phone,
+      },
+      code
+    );
 
+    res.json(participant);
+  },
 
-        if(!name) throw new AppErrorMissing('name')
-        if(!surname) throw new AppErrorMissing('surname')
-        if(!academicTitle) throw new AppErrorMissing('academicTitle')
-        if(!degree) throw new AppErrorMissing('degree')
-        if(!position) throw new AppErrorMissing('position')
-        if(!organization) throw new AppErrorMissing('organization')
-        if(!phone) throw new AppErrorMissing('phone')
+  async checkEmail({ body: { email, code } }, res) {
+    if (!email) throw new AppErrorMissing("email");
+    if (!code) throw new AppErrorMissing("code");
 
-        const code = randomCode(6, '0123456789');
+    const participant = await authService.checkEmail(email, code);
+    res.json(participant);
+  },
 
-        const hashPassword= bcrypt.hashSync(password, 10)
-        const participant =await authService.register({
-            email, hashPassword, name, surname, patronymic, academicTitle, degree, position, organization, phone
-        }, code)
+  async loginSfedu(req, res) {},
+  async reset(
+    { body: { currentPassword, newPassword, repeatPassword }, user },
+    res
+  ) {
+    if (!newPassword !== repeatPassword) throw AppErrorInvalid("newPassword");
+    await authService.resetPassword(
+      { currentPassword, newPassword, repeatPassword },
+      user
+    );
 
-        res.json(participant)
-    },
-
-    async checkEmail({body: { email, code }}, res) {
-        if(!email) throw new AppErrorMissing('email')
-        if(!code) throw new AppErrorMissing('code')
-        const participant= await authService.checkEmail(email, code)
-        res.json(participant)
-    },
-
-    async loginSfedu(req, res) {
-
-    }
-    ,
-
-    async reset({body: {currentPassword, newPassword, repeatPassword }, user}, res) {
-
-        if(!newPassword !== repeatPassword) throw AppErrorInvalid('newPassword')
-        await  authService.resetPassword({currentPassword, newPassword, repeatPassword },user )
-
-        res.json({status: 'ok'})
-    }
-
-}
+    res.json({ status: "ok" });
+  },
+};
