2b11e577ffc1925cb08ca46bcfcdef2dda1f81dc
Никита <100412266+Niks6455@users.noreply.github.com>
2025-02-17 16:26:28 +0300

    asd

--- front/src/components/AdminModuleComponents/AddOrgPeople/AddOrgPeople.jsx
+++ front/src/components/AdminModuleComponents/AddOrgPeople/AddOrgPeople.jsx
@@ -1,9 +1,27 @@
 import styles from "./AddOrgPeople.module.scss";
-
+import addPhoto from "@assets/img/AdminPanel/addPhoto.svg";
+import deletePhotoImg from "@assets/img/AdminPanel/delete.svg";
 function AddOrgPeople() {
     return ( 
         <div className={styles.AddOrgPeople}>
-AddOrgPeople
+            <div className={styles.AddOrgPeopleInner}>
+                <div className={styles.AddOrgPeopleFile}>
+                    
+                </div>
+                <input style={{display: "none"}} type="file" />
+                <div className={styles.AddOrgPeopleInput}>
+                    <label>ФИО</label>
+                    <input type="text"/>
+                </div>
+                <div className={styles.AddOrgPeopleInput}>
+                    <label>Организация</label>
+                    <textarea type="text"/>
+                </div>
+                <div className={styles.AddOrgPeopleButton}>
+                    <button className={styles.save}>Сохранить</button>
+                    <button className={styles.delete}><img src={deletePhotoImg}/></button>
+                </div>
+            </div>
         </div>
      );
 }
