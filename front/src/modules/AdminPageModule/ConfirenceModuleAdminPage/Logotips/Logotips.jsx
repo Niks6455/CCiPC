fbc40da1bacee07b6dd80f5b0e9a3c7a5813455d
Artur <artur.tonoyan.04@list.ru>
2025-02-21 20:06:30 +0300

    1

--- front/src/modules/AdminPageModule/ConfirenceModuleAdminPage/Logotips/Logotips.jsx
+++ front/src/modules/AdminPageModule/ConfirenceModuleAdminPage/Logotips/Logotips.jsx
@@ -4,11 +4,8 @@ function Logotips() {
   return (
     <div className={styles.Logotips}>
       <div className={styles.left_block}>
-        <div className={styles.border}></div>
-        <div className={styles.block_inner}>
-          <h3>Логотип (хедер)</h3>
-          <div className={styles.container}></div>
-        </div>
+        <h3>Логотип (хедер)</h3>
+        <div className={styles.container}></div>
       </div>
       <div className={styles.rigth_block}>
         <h3>Логотип (футер)</h3>
