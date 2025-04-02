4064bed1f690702329c47884b511ac04f9af7573
Igor <tro@rdcenter.ru>
2025-04-02 11:55:45 +0300

    fix

--- api/controllers/participant.js
+++ api/controllers/participant.js
@@ -7,6 +7,27 @@ function validateName(name) {
     return name.length > 0 && name.length <= 50 && nameRegex.test(name);
 }
 
+function validatePhoneNumber(phone) {
+    // Удаляем пробелы и дефисы
+    const cleanedPhone = phone.replace(/[\s-]+/g, '');
+
+    // Проверяем, соответствует ли номер формату +7 и 10 цифрам
+    const phoneRegex = /^\+7\d{10}$/;
+
+    return phoneRegex.test(cleanedPhone);
+}
+
+
+function validateOrganization(name) {
+    // Удаляем пробелы по краям и проверяем, что строка не пустая
+    const trimmedName = name.trim();
+
+    // Проверяем, соответствует ли название требованиям
+    const nameRegex = /^[a-zA-Zа-яА-ЯёЁ0-9.,'"() -]{3,200}$/;
+
+    return trimmedName.length > 0 && nameRegex.test(trimmedName);
+}
+
 export default {
 
     async self({ user }, res){
@@ -23,6 +44,9 @@ export default {
         if(name && !validateName(name)) throw new AppErrorInvalid('name')
         if(surname && !validateName(surname)) throw new AppErrorInvalid('surname')
         if(patronymic && !validateName(patronymic)) throw new AppErrorInvalid('patronymic')
+
+        if(phone && !validatePhoneNumber(phone)) throw new AppErrorInvalid('phone')
+        if(organization && !validateOrganization(organization)) throw new AppErrorInvalid('organization')
 
         if(formPay &&  !['Безналичный', 'Наличный', 'Не выбран'].includes(formPay)) throw  new AppErrorInvalid('formPay')
 
