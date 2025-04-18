91402cce6039a4f7ce2612349af81fee2a191781
Artur <artur.tonoyan.04@list.ru>
2025-04-18 13:49:05 +0300

    1

--- front/src/modules/AdminPageModule/OrgWznosModuleAdminPage/data.js
+++ front/src/modules/AdminPageModule/OrgWznosModuleAdminPage/data.js
@@ -36,20 +36,3 @@ export const tableHeader = [
     key: 'confirmation',
   },
 ];
-
-export const testData = Array(10)
-  .fill(0)
-  .map((_, i) => ({
-    fio: `Иванов Иван Иванович ${i}`,
-    report:
-      'Проект по созданию распределенной сети полигонов для отработки сценариев применения гетерогенных групп транспортных средств с электрическим приводом в сложных климатических и ландшафтных условиях, проблемы и перспективы реализации на примере САО РАН',
-    organization: 'Санкт-Петербургский государственный электротехнический университет «ЛЭТИ»',
-    participationForm: 'Очно',
-    participationStatus: 'Аспирант',
-    paymentForm: 'Безналичный расчёт',
-    sumOrgWznos: 2201,
-    confirmation: false,
-    author: false,
-    contract: null,
-    receipt: null,
-  }));
--- front/src/modules/DeleteAccount/DeleteAccount.jsx
+++ front/src/modules/DeleteAccount/DeleteAccount.jsx
@@ -4,7 +4,13 @@ import style from './DeleteAccount.module.scss';
 import { useContext } from 'react';
 import DataContext from '../../context';
 import { apiDeleteAccount } from '../../apirequests/apirequests';
+import { useSelector } from 'react-redux';
+import { convertDateTire } from '../../utils/functions/funcions';
+import circleRed from '@assets/img/circleRed.svg';
+
 function DeleteAccount() {
+  const dedlineReport2 = useSelector(state => state?.conferences?.data[0]?.dedlineReport2);
+  const reports = useSelector(state => state?.reportsSlice?.data);
   const navigate = useNavigate();
   const context = useContext(DataContext);
   const funDeleteAccaunt = () => {
@@ -18,26 +24,47 @@ function DeleteAccount() {
   };
   return (
     <section className={style.DeleteAccount}>
-      <div>
-        <div className={style.DeleteAccountImg}>
-          <img src={deleteImg} />
+      {new Date(convertDateTire(dedlineReport2)) > new Date() && reports?.length === 0 ? (
+        <div>
+          <div className={style.DeleteAccountImg}>
+            <img src={deleteImg} />
+          </div>
+          <p>
+            Все данные будут утеряны навсегда. <br />
+            Вы уверены, что хотите удалить аккаунт?
+          </p>
+          <div className={style.DeleteAccountButton}>
+            <button
+              onClick={() => {
+                navigate('/account/profile');
+                context.setSelectFrameLks('profile');
+              }}
+            >
+              Нет
+            </button>
+            <button onClick={funDeleteAccaunt}>Да</button>
+          </div>
         </div>
-        <p>
-          Все данные будут утеряны навсегда. <br />
-          Вы уверены, что хотите удалить аккаунт?
-        </p>
-        <div className={style.DeleteAccountButton}>
-          <button
-            onClick={() => {
-              navigate('/account/profile');
-              context.setSelectFrameLks('profile');
-            }}
-          >
-            Нет
-          </button>
-          <button onClick={funDeleteAccaunt}>Да</button>
+      ) : (
+        <div>
+          <p className={style.nodelete}>
+            Удалить акаунт можно после <br /> завершения конференции!
+          </p>
+          <div className={style.nodeleteImg}>
+            <img src={circleRed} alt=":(" />
+          </div>
+          <div className={style.goProfile}>
+            <button
+              onClick={() => {
+                navigate('/account/profile');
+                context.setSelectFrameLks('profile');
+              }}
+            >
+              Перейти в профиль
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
