daff84fe61c022111db46b5621fc35436b05593b
Igor <tro@rdcenter.ru>
2025-05-06 19:25:07 +0300

    add № in organization

--- api/utils/validate/organization.js
+++ api/utils/validate/organization.js
@@ -1,5 +1,5 @@
 export default (organization) => {
     const trimmedName = organization.trim();
-    const nameRegex = /^[a-zA-Zа-яА-ЯёЁ0-9.,'"() -]{3,200}$/;
+    const nameRegex = /^[a-zA-Zа-яА-ЯёЁ0-9.,'"() -№]{3,200}$/;
     return trimmedName.length > 0 && nameRegex.test(trimmedName);
 }
